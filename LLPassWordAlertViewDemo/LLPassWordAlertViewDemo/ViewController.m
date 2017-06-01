ecb3ba6c1191446bbacd7a7fe93a63eef435ca00
lilongWork <lilongcnc@vip.qq.com>
2017-06-01 15:33:06 +0800

    delete disused codes

--- LLPassWordAlertViewDemo/LLPassWordAlertViewDemo/ViewController.m
+++ LLPassWordAlertViewDemo/LLPassWordAlertViewDemo/ViewController.m
@@ -25,8 +25,12 @@
         
         NSLog(@"输入密码完成:%@",pwStr);
         
+    } canelBtnOnClick:^(){
+        //取消显示按钮被点击
+        NSLog(@"取消按钮被点击");
+
     }];
-    
+
 }
 
 
@@ -35,9 +39,11 @@
 {
     
     [LLPassWordAlertView showWithTitle:@"验证密码" desStr:@"请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码请输入支付密码" finish:^(NSString *pwStr) {
-        
         NSLog(@"输入密码完成:%@",pwStr);
-        
+    } canelBtnOnClick:^(){
+        //取消显示按钮被点击
+        NSLog(@"取消按钮被点击");
+
     }];
 }
 
